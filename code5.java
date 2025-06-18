8e93eb1092bbcc2cdb15c178f0aec7c3b2058864
Rajesh451807 <rajeshpalla489@gmail.com>
2025-06-18 12:25:25 +0530

    Create code5.java

---
+++ code5.java
@@ -0,0 +1,13 @@
+class Patterns5{
+public static void main(String []args){
+int n=1;
+for(int i=1;i<=5;i++){
+for(int j=0;j<5;j++)
+{
+System.out.print(n + " ");
+n=n+2;
+}
+System.out.println();
+}
+}
+}
